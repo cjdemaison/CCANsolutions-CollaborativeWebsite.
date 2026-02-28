6022732eaf9fb0a35f40e45f7c14eac65f89b6cc
cjdemaison <jamesdemaison@yahoo.com>
2026-02-28 09:26:24 -0500

    Update add_lead.php

--- api/add_lead.php
+++ api/add_lead.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . "/db.php";
+
+require_once __DIR__ . '/../require_login.php';
+require_once __DIR__ . '/../db.php';
 
 $raw = file_get_contents("php://input");
 $data = json_decode($raw, true);
